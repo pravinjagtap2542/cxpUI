38b91a315fde40f27aea154d5ecc7f368e9e277a
Sunny Sehgal <Sunny.Sehgal@INFOVISIONLABS.COM>
2020-05-18 16:01:14 +0530

    Edit My Profile page added

--- src/app/views/user-profile/edit-my-profile/edit-my-profile.component.ts
+++ src/app/views/user-profile/edit-my-profile/edit-my-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-edit-my-profile',
@@ -10,6 +11,23 @@ export class EditMyProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
   }
 
+  openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    if(evt){
+      evt.currentTarget.className += " active";
+    }
+  }
 }
